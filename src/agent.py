1c9927c2ca27adde6745f50fb111dfdd7ed1bc46
Anubhavdas651 <23mc3010@rgipt.ac.in>
2026-04-03 22:42:44 +0530

    updates

--- src/agent.py
+++ src/agent.py
@@ -4,4 +4,5 @@ from scraper import fetch_website_contents, fetch_website_links
 OLLAMA_BASE_URL = "http://localhost:11434/v1"
 MODEL = "llama3.2"
 
-links = fetch_website_links("https://github.com/AnubhavDas651629/ai-brochure-generator/blob/main/src/agent.py")
+links = fetch_website_links("https://edwarddonner.com")
+links

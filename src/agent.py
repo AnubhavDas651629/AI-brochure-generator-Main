a0f5d73099341ff37f9d226517c3f63cbf6eff4f
Anubhavdas651 <23mc3010@rgipt.ac.in>
2026-04-03 22:58:34 +0530

    brochure

--- src/agent.py
+++
@@ -1,8 +0,0 @@
-from openai import OpenAI
-from scraper import fetch_website_contents, fetch_website_links
-
-OLLAMA_BASE_URL = "http://localhost:11434/v1"
-MODEL = "llama3.2"
-
-links = fetch_website_links("https://edwarddonner.com")
-links
